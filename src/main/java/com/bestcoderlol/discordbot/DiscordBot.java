d2fdd6d8f1e916e9c174ef853c7fca684f46064f
GD <gd1902yt@gmail.com>
2019-01-11 21:50:34 +0100

    bot token

---
+++ src/main/java/com/bestcoderlol/discordbot/DiscordBot.java
@@ -0,0 +1,10 @@
+package com.bestcoderlol.discordbot;
+
+import net.dv8tion.jda.core.JDABuilder;
+
+public class DiscordBot {
+    public static void main(String[] args) {
+        JDABuilder jdaB = new JDABuilder();
+        
+    }
+}
